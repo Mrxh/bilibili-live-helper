7a1b470179e67eecabeea2ebc350cc5ebd11d7c4
ayang <473033518@qq.com>
2022-07-13 00:28:21 +0800

    :art: the song assistant increases the dom prompt of the volume change

--- src/hooks/useMusicInfo.ts
+++ src/hooks/useMusicInfo.ts
@@ -1,249 +1,250 @@
-import { onMounted, ref, watch, computed } from 'vue'
-import { ipcRenderer } from 'electron'
-import Lyric from 'lrc-file-parser'
+import { onMounted, ref, watch, computed } from "vue";
+import { ipcRenderer } from "electron";
+import Lyric from "lrc-file-parser";
 import {
-  searchPlaylistInfoApi,
-  searchMusicInfoApi,
-  searchLyricApi,
-  getMusicUrl
-} from '@/api'
-import { Volume } from '@/constants'
-import { getStore, setStore } from '@/stores/electron'
-import type { currentMusicInfo, SongPlayItem } from '@/types'
+	searchPlaylistInfoApi,
+	searchMusicInfoApi,
+	searchLyricApi,
+	getMusicUrl,
+} from "@/api";
+import { Volume } from "@/constants";
+import { getStore, setStore } from "@/stores/electron";
+import type { currentMusicInfo, SongPlayItem } from "@/types";
 
 const useMusicInfo = () => {
-  // 创建一个 audio 对象
-  const audio = new Audio()
-  // 正在播放的音乐信息
-  const currentPlaySong = ref<currentMusicInfo>()
-  // 音量
-  const volume = ref<number>(getStore(Volume))
-  // 音乐是否播放
-  const isPlay = ref<boolean>(false)
-  // 随机播放歌单列表
-  const randomPlayList = ref<any[]>([])
-  // 点歌的歌单列表
-  const songPlayList = ref<SongPlayItem[]>([])
-  // 点歌的歌单列表的播报 id
-  const currentBroadcastIndex = ref(1)
-  // 切歌列表 记录 uid
-  const cutSongList = ref<number[]>([])
-  // 处理歌词的对象
-  const lyricTools = ref<any>()
-  // 音乐进度
-  const musicProgress = ref<number>(0)
+	// 创建一个 audio 对象
+	const audio = new Audio();
+	// 正在播放的音乐信息
+	const currentPlaySong = ref<currentMusicInfo>();
+	// 音量
+	const volume = ref<number>(getStore(Volume));
+	// 音乐是否播放
+	const isPlay = ref<boolean>(false);
+	// 随机播放歌单列表
+	const randomPlayList = ref<any[]>([]);
+	// 点歌的歌单列表
+	const songPlayList = ref<SongPlayItem[]>([]);
+	// 点歌的歌单列表的播报 id
+	const currentBroadcastIndex = ref(1);
+	// 切歌列表 记录 uid
+	const cutSongList = ref<number[]>([]);
+	// 处理歌词的对象
+	const lyricTools = ref<any>();
+	// 音乐进度
+	const musicProgress = ref<number>(0);
 
-  // 音乐图片元素样式
-  const coverStyle = computed(() => {
-    return {
-      animationPlayState: `${isPlay.value ? 'running' : 'paused'}`
-    }
-  })
+	// 音乐图片元素样式
+	const coverStyle = computed(() => {
+		return {
+			animationPlayState: `${isPlay.value ? "running" : "paused"}`,
+		};
+	});
 
-  // 获取随机歌单列表
-  const getRandomPlayList = async () => {
-    // 初始话获取热门榜单列表随机播放
-    const result = await searchPlaylistInfoApi()
+	// 获取随机歌单列表
+	const getRandomPlayList = async () => {
+		// 初始话获取热门榜单列表随机播放
+		const result = await searchPlaylistInfoApi();
 
-    if (!result) return
+		if (!result) return;
 
-    randomPlayList.value = result.playlist.tracks
+		randomPlayList.value = result.playlist.tracks;
 
-    playMusic()
-  }
+		playMusic();
+	};
 
-  // 处理歌词
-  const handleLyric = (lyric: string) => {
-    lyricTools.value = new Lyric({
-      onPlay (line, text) {
-				currentPlaySong.value!.lyric = text as string
-      },
-      offset: 150,
-      isRemoveBlankLine: true,
-      lyric,
-      translationLyric: '',
-      onSetLyric (lines) {}
-    })
+	// 处理歌词
+	const handleLyric = (lyric: string) => {
+		lyricTools.value = new Lyric({
+			onPlay(line, text) {
+				currentPlaySong.value!.lyric = text as string;
+			},
+			offset: 150,
+			isRemoveBlankLine: true,
+			lyric,
+			translationLyric: "",
+			onSetLyric(lines) {},
+		});
 
-    lyricTools.value.setLyric(lyric)
-  }
+		lyricTools.value.setLyric(lyric);
+	};
 
-  // 筛选要播放的歌曲
-  const playMusic = async () => {
-    isPlay.value = false
+	// 筛选要播放的歌曲
+	const playMusic = async () => {
+		isPlay.value = false;
 
-    if (currentPlaySong.value?.cover) currentPlaySong.value!.cover = ''
+		if (currentPlaySong.value?.cover) currentPlaySong.value!.cover = "";
 
-    let info: any = {}
+		let info: any = {};
 
-    if (songPlayList.value.length) {
-      const { uid, uname, musicName } = songPlayList.value[0]
+		if (songPlayList.value.length) {
+			const { uid, uname, musicName } = songPlayList.value[0];
 
-      // 获取点歌列表第一首的信息
-      const getMusicInfo = await searchMusicInfoApi(musicName)
+			// 获取点歌列表第一首的信息
+			const getMusicInfo = await searchMusicInfoApi(musicName);
 
-      if (!getMusicInfo) {
-        playMusic()
+			if (!getMusicInfo) {
+				playMusic();
 
-        return
-      }
+				return;
+			}
 
-      // 将用户信息也放进去，播放失败可以发消息让换一首歌曲
-      info = { uid, uname, ...getMusicInfo.result.songs[0] }
+			// 将用户信息也放进去，播放失败可以发消息让换一首歌曲
+			info = { uid, uname, ...getMusicInfo.result.songs[0] };
 
-      // 删除掉列表第一首
-      songPlayList.value.shift()
-    } else if (randomPlayList.value.length) {
-      // 随机获取一首歌曲
-      info =
+			// 删除掉列表第一首
+			songPlayList.value.shift();
+		} else if (randomPlayList.value.length) {
+			// 随机获取一首歌曲
+			info =
 				randomPlayList.value[
-				  Math.floor(Math.random() * randomPlayList.value.length)
-				]
+					Math.floor(Math.random() * randomPlayList.value.length)
+				];
 
-      // 从随机歌单中删除
-      const findIndex = randomPlayList.value.findIndex(
-        (item) => item.id === info.id
-      )
-      randomPlayList.value.splice(findIndex, 1)
-    } else {
-      getRandomPlayList()
+			// 从随机歌单中删除
+			const findIndex = randomPlayList.value.findIndex(
+				(item) => item.id === info.id
+			);
+			randomPlayList.value.splice(findIndex, 1);
+		} else {
+			getRandomPlayList();
 
-      return
-    }
+			return;
+		}
 
-    if (songPlayList.value.length <= 1) {
-      currentBroadcastIndex.value = 1
-    }
+		if (songPlayList.value.length <= 1) {
+			currentBroadcastIndex.value = 1;
+		}
 
-    handlePlay(info)
-  }
+		handlePlay(info);
+	};
 
-  // 处理播放
-  const handlePlay = async (music: any) => {
-    const { uid, uname, id, name, al, ar, dt } = music
+	// 处理播放
+	const handlePlay = async (music: any) => {
+		const { uid, uname, id, name, al, ar, dt } = music;
 
-    // 获取歌词
-    const getLyric = await searchLyricApi(id)
-    if (!getLyric) return
+		// 获取歌词
+		const getLyric = await searchLyricApi(id);
+		if (!getLyric) return;
 
-    let hasLyric = false
-    let lyric = '纯音乐，请欣赏'
+		let hasLyric = false;
+		let lyric = "纯音乐，请欣赏";
 
-    // needDesc 为 true 无歌词 反之
-    if (!getLyric?.needDesc) {
-      hasLyric = true
+		// needDesc 为 true 无歌词 反之
+		if (!getLyric?.needDesc) {
+			hasLyric = true;
 
-      lyric = getLyric.lrc.lyric
-    }
+			lyric = getLyric.lrc.lyric;
+		}
 
-    currentPlaySong.value = {
-      uid,
-      uname,
-      id,
-      name,
-      singer: ar.map((item: any) => item.name).join(' / '),
-      cover: al.picUrl,
-      currentDuration: 0,
-      totalDuration: +dt.toString().slice(0, -3),
-      hasLyric
-    }
-    console.log('volume.value', volume.value)
-    audio.src = getMusicUrl(id)
-    audio.volume = volume.value
+		currentPlaySong.value = {
+			uid,
+			uname,
+			id,
+			name,
+			singer: ar.map((item: any) => item.name).join(" / "),
+			cover: al.picUrl,
+			currentDuration: 0,
+			totalDuration: +dt.toString().slice(0, -3),
+			hasLyric,
+		};
+		audio.src = getMusicUrl(id);
+		audio.volume = volume.value;
 
-    // 可以正常播放且无需停顿时播放音乐
-    audio.oncanplaythrough = () => {
-      isPlay.value = true
-      handleLyric(lyric)
-    }
+		// 可以正常播放且无需停顿时播放音乐
+		audio.oncanplaythrough = () => {
+			isPlay.value = true;
+			handleLyric(lyric);
+		};
 
-    // 监听音乐时间变化
-    audio.ontimeupdate = () => {
-			currentPlaySong.value!.currentDuration = audio.currentTime
-			musicProgress.value = audio.currentTime
-    }
+		// 监听音乐时间变化
+		audio.ontimeupdate = () => {
+			currentPlaySong.value!.currentDuration = audio.currentTime;
+			musicProgress.value = audio.currentTime;
+		};
 
-    // 加载错误时播放下一首
-    audio.onerror = () => {
-      playMusic()
-    }
+		// 加载错误时播放下一首
+		audio.onerror = () => {
+			playMusic();
+		};
 
-    // 播放完后去找下一首音乐
-    audio.onended = () => {
-      playMusic()
-    }
-  }
+		// 播放完后去找下一首音乐
+		audio.onended = () => {
+			playMusic();
+		};
+	};
 
-  // 处理音乐进度
-  const handleProgress = (time: number | number[]) => {
-    time = time as number
+	// 处理音乐进度
+	const handleProgress = (time: number | number[]) => {
+		time = time as number;
 
-    audio.currentTime = time
-    musicProgress.value = time
-  }
+		audio.currentTime = time;
+		musicProgress.value = time;
+	};
 
-  // 处理音量
-  const handleVolume = (event: WheelEvent) => {
-    const sign = event.deltaY > 0 ? 1 : -1
-    const target = Number((audio.volume + sign * 0.01).toFixed(2))
+	// 处理音量
+	const handleVolume = (event: WheelEvent) => {
+		const sign = event.deltaY > 0 ? 1 : -1;
+		const target = Number((audio.volume + sign * 0.01).toFixed(2));
 
-    audio.volume = sign > 0 ? Math.min(1, target) : Math.max(0, target)
+		audio.volume = sign > 0 ? Math.min(1, target) : Math.max(0, target);
 
-    volume.value = audio.volume
+		volume.value = audio.volume;
 
-    setStore(Volume, volume.value)
-  }
+		setStore(Volume, volume.value);
+	};
 
-  onMounted(() => {
-    // 监听点|切歌消息
-    ipcRenderer.on('listen-sone-barrage', (_, barrage) => {
-      const { message, uname, uid } = barrage
-      // 点歌操作
-      // TODO: 加个歌曲黑名单 lost rivers Early Steps
-      if (message.includes('点歌')) {
-        const musicName = message.replace('点歌', '').trim()
-        // 如果歌曲名为空，无操作
-        if (!musicName) return
+	onMounted(() => {
+		// 监听点|切歌消息
+		ipcRenderer.on("listen-sone-barrage", (_, barrage) => {
+			const { message, uname, uid } = barrage;
+			// 点歌操作
+			// TODO: 加个歌曲黑名单 lost rivers Early Steps
+			if (message.includes("点歌")) {
+				const musicName = message.replace("点歌", "").trim();
+				// 如果歌曲名为空，无操作
+				if (!musicName) return;
 
-        songPlayList.value.push({ uid, uname, musicName })
-      } else {
-        // 切歌操作
-        cutSongList.value = [...new Set([...cutSongList.value, uid])]
-        // TODO: 切歌操作设置为置顶人数
-        if (cutSongList.value.length >= 3) {
-          playMusic()
-          cutSongList.value.splice(0)
-        }
-      }
-    })
+				songPlayList.value.push({ uid, uname, musicName });
+			} else {
+				// 切歌操作
+				cutSongList.value = [...new Set([...cutSongList.value, uid])];
 
-    getRandomPlayList()
-  })
+				if (cutSongList.value.length >= 3) {
+					playMusic();
 
-  watch(isPlay, (newValue) => {
-    if (newValue) {
-      audio.play()
-      lyricTools.value.play(
+					cutSongList.value.splice(0);
+				}
+			}
+		});
+
+		getRandomPlayList();
+	});
+
+	watch(isPlay, (newValue) => {
+		if (newValue) {
+			audio.play();
+			lyricTools.value.play(
 				currentPlaySong.value!.currentDuration * 1000
-      )
-    } else {
-      audio.pause()
-      lyricTools.value.pause()
-    }
-  })
+			);
+		} else {
+			audio.pause();
+			lyricTools.value.pause();
+		}
+	});
 
-  return {
-    currentPlaySong,
-    songPlayList,
-    currentBroadcastIndex,
-    cutSongList,
-    isPlay,
-    coverStyle,
-    playMusic,
-    musicProgress,
-    handleProgress,
-    handleVolume
-  }
-}
+	return {
+		currentPlaySong,
+		songPlayList,
+		currentBroadcastIndex,
+		cutSongList,
+		isPlay,
+		coverStyle,
+		playMusic,
+		musicProgress,
+		handleProgress,
+		handleVolume,
+		volume,
+	};
+};
 
-export default useMusicInfo
+export default useMusicInfo;
