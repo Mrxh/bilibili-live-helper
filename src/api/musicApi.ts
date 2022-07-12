908fba8ded5a7ad9188734bddefff0c560355c2b
Kaichen <276126767@qq.com>
2022-07-12 13:10:09 +0200

    feat: 增加检查音乐是否可用api

--- src/api/musicApi.ts
+++ src/api/musicApi.ts
@@ -1,8 +1,13 @@
 import getQueryData from '@/utils/request'
 
+// 网易云请求url
+const baseUrl = 'https://music-node.vercel.app'
+// 默认加载的歌单id
+const defaultPlaylist = 3778678
+
 // 查找歌单信息
-const searchPlaylistInfoApi = async (id: number) =>
-  await getQueryData('https://music-node.vercel.app/playlist/detail', {
+const searchPlaylistInfoApi = async (id: number = defaultPlaylist) =>
+  await getQueryData(`${baseUrl}/playlist/detail`, {
     params: {
       id
     }
@@ -10,15 +15,23 @@ const searchPlaylistInfoApi = async (id: number) =>
 
 // 搜索音乐信息
 const searchMusicInfoApi = async (keywords: string) =>
-  await getQueryData('https://music-node.vercel.app/cloudsearch', {
+  await getQueryData(`${baseUrl}/cloudsearch`, {
     params: {
       keywords
     }
   })
 
+// 判断歌曲是否可以播放
+const isMusicPlayableApi = async (id: number) =>
+  await getQueryData(`${baseUrl}/check/music`, {
+    params: {
+      id
+    }
+  })
+
 // 搜索歌词
 const searchLyricApi = async (id: number) =>
-  await getQueryData('https://music-node.vercel.app/lyric', {
+  await getQueryData(`${baseUrl}/lyric`, {
     params: {
       id
     }
@@ -27,5 +40,6 @@ const searchLyricApi = async (id: number) =>
 export {
   searchPlaylistInfoApi,
   searchMusicInfoApi,
+  isMusicPlayableApi,
   searchLyricApi
 }
